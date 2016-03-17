f88df25c354b2f1700c5b5e4a2a6dbb591d6a010
Chetan Gaonker <chetang@calsoftlabs.com>
2016-03-17 00:14:12 -0700

    Removing utils import from md5 tests because of __init__ in test directory

--- src/test/md5/md5AuthTest.py
+++ src/test/md5/md5AuthTest.py
@@ -1,8 +1,5 @@
 import unittest
 import os,sys
-CORD_TEST_UTILS = 'utils'
-test_root = os.getenv('CORD_TEST_ROOT') or './'
-sys.path.append(test_root + CORD_TEST_UTILS)
 from EapMD5 import MD5AuthTest
 
 class eap_auth_exchange(unittest.TestCase):
