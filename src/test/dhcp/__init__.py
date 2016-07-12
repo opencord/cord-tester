10fe00edbedc34c149a6b8c28b65a78831e08de5
A R Karthick <kramanar@ciena.com>
2016-07-12 16:13:16 -0700

    Fix: Use portmap or config for dhcp interface over hard coded interface
    Change-Id: I13d7d3066cb8a7690c7915a4df1e4651c469b180

--- src/test/dhcp/__init__.py
+++ src/test/dhcp/__init__.py
@@ -1,12 +1,12 @@
-# 
+#
 # Copyright 2016-present Ciena Corporation
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 # http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -18,5 +18,8 @@ import os,sys
 working_dir = os.path.dirname(os.path.realpath(sys.argv[-1]))
 utils_dir = os.path.join(working_dir, '../utils')
 fsm_dir = os.path.join(working_dir, '../fsm')
+subscriber_dir = os.path.join(working_dir, '../subscriber')
 __path__.append(utils_dir)
 __path__.append(fsm_dir)
+__path__.append(subscriber_dir)
+
