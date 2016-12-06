0869a32a98b6f118ff130fc1774db2e3b2387496
You Wang <you@onlab.us>
2016-12-05 16:55:51 -0800

    API test cases for /core/services
    Change-Id: Icbba49162d07707298e1b0a6e8607e61879b3756

--- src/test/cord-api/Properties/RestApiProperties.py
+++ src/test/cord-api/Properties/RestApiProperties.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 SERVER_IP = '10.128.100.9'
-SERVER_PORT = '8888'
+SERVER_PORT = '8080'
 USER = 'padmin@vicci.org'
 PASSWD = 'letmein'
 TENANT_SUBSCRIBER = '/api/tenant/cord/subscriber/'
@@ -8,3 +8,4 @@ TENANT_VOLT = '/api/tenant/cord/volt/'
 UTILS_SYNCHRONIZER = '/api/utility/synchronizer/'
 UTILS_LOGIN = '/api/utility/login/'
 CORE_USERS = '/api/core/users/'
+CORE_SERVICES = '/api/core/services/'
