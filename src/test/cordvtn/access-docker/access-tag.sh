8477c96bf3c7700b297bc11cf4bd75bd5e8c1012
Chetan Gaonker <chgaonke@partner.ciena.com>
2017-01-23 17:39:45 +0000

    Removing access docker folder, added earlier for trial of manual tests for cord vtn.
    Change-Id: I99f676620ea57ed5616cea7be4f4ce0868090a5d

--- src/test/cordvtn/access-docker/access-tag.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-sudo docker run --privileged --cap-add=ALL -d -v /dev:/dev -v /lib/modules:/lib/modules --name access -t vlan /bin/bash
-sudo ./pipework fabric -i eth1 access 10.168.0.254/24
-sudo docker exec -d access modprobe 8021q
-sudo docker exec -d access vconfig add eth1 222
-sudo docker exec -d access ip link set eth1.222 up
-sudo docker exec -d access ip addr add 10.169.0.254/24 dev eth1.222
--- src/test/cordvtn/access-docker/run-agent.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-sudo docker run --privileged --cap-add=ALL -d --name access-agent -t ubuntu:14.04 /bin/bash
-sudo ./pipework br-mgmt -i eth1 access 10.10.10.101/24
-sudo ./pipework br-int -i eth2 access 10.168.0.101/24
