48d3d2d40af8703d2117b7957540736f5983bcb1
Matteo Scandolo <matteo.scandolo@gmail.com>
2017-08-08 13:05:27 -0700

    [CORD-1685] Adding copyright informations
    Change-Id: Ia91da2250acc793ac0a9df2844aa30a9fc6890a9

--- src/test/setup/onos_pull.sh
+++ src/test/setup/onos_pull.sh
@@ -1,3 +1,19 @@
+
+# Copyright 2017-present Open Networking Foundation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
 #!/usr/bin/env bash
 ##Use this script as SUDO to pull ONOS safely as it saves/archives repo digest ids.
 ##Repo digest ids are saved in $HOME/onos_repo_digest.txt
