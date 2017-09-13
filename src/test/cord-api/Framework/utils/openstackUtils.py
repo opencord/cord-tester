c713e45847bd378a3b734e97bc32e4df8514519c
Kailash Khalasi <kailash@onlab.us>
2017-09-13 13:37:14 -0700

    updating cord vtn tests
    Change-Id: Iefbc96527d1636552de1d4f91c080ccce3efd490
    (cherry picked from commit f6de2594c632f74156d29d68984003171c04db86)

--- src/test/cord-api/Framework/utils/openstackUtils.py
+++ src/test/cord-api/Framework/utils/openstackUtils.py
@@ -54,3 +54,24 @@ def get_nova_lists(novalist,nameWildCard=None):
                 return instances[key]
     else:
         return instances
+
+def get_instance_status(novalist,nameWildCard=None):
+    pairs = re.split("\+-*\+-*\+\n?",novalist)[2:-1]
+    ids,names,status,taskState,powerState,networks = [],[],[],[],[],[]
+    for p in pairs:
+      for l in p.split('\n'):
+        pair = l.split('|')
+        if len(pair) > 1:
+          ids.append(pair[1].strip())
+          names.append(pair[2].strip())
+          status.append(pair[3].strip())
+          taskState.append(pair[4].strip())
+          powerState.append(pair[5].strip())
+          networks.append(pair[6].strip())
+    instances = dict(zip(names,status))
+    if nameWildCard is not None:
+        for key in instances.keys():
+            if re.match(nameWildCard, key):
+                return instances[key]
+    else:
+        return instances
