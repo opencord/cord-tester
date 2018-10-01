d4e0532d91cfda4d04877c2d554c1c49183d9405
Kailash Khalasi <kailash@onlab.us>
2018-10-01 10:12:40 -0700

    adding cord-tester venv script
    Change-Id: Id828732bf82a5d3ed4f0817c0bb699600d7f125c

---
+++ src/test/cord-api/setup_venv.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+# Copyright 2017-present Open Networking Foundation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# setup_venv_lite.sh
+# sets up a python virtualenv for running cord-tester framework tests
+
+WORKSPACE=${WORKSPACE:-.}
+VENVDIR="${WORKSPACE}/venv-cord-tester"
+
+# create venv if it's not yet there
+if [ ! -x "${VENVDIR}/bin/activate" ]; then
+  echo "Setting up dev/test virtualenv in ${VENVDIR} for CORD-TESTER"
+  virtualenv -q "${VENVDIR}" --no-site-packages
+  echo "Virtualenv created."
+fi
+
+echo "Installing python requirements in virtualenv with pip"
+source "${VENVDIR}/bin/activate"
+pip install --upgrade pip
+pip install robotframework robotframework-requests robotframework-sshlibrary pexpect  \
+    robotframework-httplibrary robotframework-kafkalibrary pygments pyyaml
+pip install requests
+
+echo "CORD-TESTER virtualenv created. Run 'source ${VENVDIR}/bin/activate'."
